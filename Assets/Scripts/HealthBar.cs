a7028a0645237f5fce79e959b9c652d4825a2d14
Jimmy Lam <jimmyl@uoregon.edu>
2021-05-07 21:44:31 -0700

    added health bar and made vehicle always point forward

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,12 +7,12 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
-    public Gradient gradient;
+    public Gradient gradient;  // changes the fill color from green to red
     public Image fill;
 
     public void setHealth(float val)
     {
         slider.value = val;
-        fill.color = gradient.Evaluate(val);
+        fill.color = gradient.Evaluate(val); 
     }
 }
