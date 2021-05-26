1eb90149049082c0f0786b98e09ff4cec3e00112
Jimmy Lam <jimmyl@uoregon.edu>
2021-05-26 10:11:29 -0700

    fixed remaining HandleCollision cases

--- Assets/Scripts/teleporter.cs
+++ Assets/Scripts/teleporter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teleporter : MonoBehaviour
+public class Teleporter : MonoBehaviour
 {
     public float loc = -20;
     public Vector3 return_pos = new Vector3(0f, -0.13f, 208f);
